d0ea211b5b7932c73b1786e051506e11458693d5
gor20009 <gor20009@byui.edu>
2025-01-18 17:29:15 -0700

    modified:   mission/index.html 	modified:   mission/mission.js 	modified:   mission/styles.css

--- mission/mission.js
+++ mission/mission.js
@@ -1,9 +1,9 @@
-const themeSelector = document.querySelectorAll('#theme-selector')
+const themeSelector = document.querySelector('#theme-selector')
 
 function changeTheme() {
     if (themeSelector.value === 'dark') {
         document.body.classList.add('dark');
-        document.querySelector('img').src = 'byu-logo_white.png'
+        document.querySelector('img').src = 'byui-logo_white.png'
     } else {
         document.body.classList.remove('dark');
         document.querySelector('img').src = 'byui-logo_blue.png'
