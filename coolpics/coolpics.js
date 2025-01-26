9b7cb417b61936b2ba1dfb5069305d18a2c363c0
gor20009 <gor20009@byui.edu>
2025-01-26 01:15:47 -0700

    modified:   coolpics/coolpics.css 	modified:   coolpics/coolpics.js 	modified:   coolpics/index.html

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,8 +1,7 @@
-const menuButton = document.querySelector("menu-button");
-const menu = document.querySelector(".menu");
-
+const menuButton = document.querySelector(".menu-button");
 function toggleMenu() {
-    menu.classList.toggle("hide");
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
 }
 
 menuButton.addEventListener("click", toggleMenu);
@@ -10,34 +9,34 @@ menuButton.addEventListener("click", toggleMenu);
 function handleResize() {
     const menu = document.querySelector(".menu");
     if (window.innerWidth > 1000) {
-        menu.classList.remove("hide");
+      menu.classList.remove("hide");
     } else {
-        menu.classList.add("hide");
+      menu.classList.add("hide");
     }
 }
-
+  
 handleResize();
 window.addEventListener("resize", handleResize);
 
 function viewerTemplate(pic, alt) {
     return `<div class="viewer">
       <button class="close-viewer">X</button>
-      <img src="${pic}" alt="${alt}">
+      <img src="norris-full.jpeg" alt="$picture">
       </div>`;
 }
 
-function viewerHandler(event) {
+function viewHandler(event) {
     const clickedElement = event.target;
-    const imgSrc = clickedElement.src.split("-")[0] + "norris-full.jpeg";
-
+    const imgSrc = clickedElement.src.split("-")[0] + "-full.jpeg"; 
+  
     const viewerHTML = viewerTemplate(imgSrc, clickedElement.alt);
     document.body.insertAdjacentHTML("afterbegin", viewerHTML);
-
+  
     const closeViewer = document.querySelector(".close-viewer");
     closeViewer.addEventListener("click", () => {
-        document.querySelector(".viewer").remove();
-    })
+      document.querySelector(".viewer").remove();
+    });
 }
-
+  
 const gallery = document.querySelector(".gallery");
 gallery.addEventListener("click", viewHandler);
